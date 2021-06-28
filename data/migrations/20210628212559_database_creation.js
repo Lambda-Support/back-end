cf2c6e44f8a1ab246010623c8b57451496c3b8b8
charlie-lu1 <charlie.m.lu1@gmail.com>
2021-06-28 14:50:44 -0700

    migration

--- data/migrations/20210628212559_database_creation.js
+++ data/migrations/20210628212559_database_creation.js
@@ -1,8 +1,63 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("users", (tbl) => {
+        tbl.increments();
+        tbl.string("name", 36)
+            .notNullable();
+        tbl.string("username", 25)
+            .notNullable()
+            .unique();
+        tbl.string("password", 36)
+            .notNullable();
+        tbl.string("role")
+            .notNullable()
+            .defaultTo("base");
+    })
+    .createTable("tickets", (tbl) => {
+        tbl.increments();
+        tbl.integer("user_id")
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("users")
+            .onUpdate('CASCADE')
+            .onDelete("CASCADE");
+        tbl.string("title")
+            .notNullable();
+        tbl.string("ticket_description")
+            .notNullable();
+        tbl.string("status")
+            .notNullable()
+            .defaultTo("New");
+        tbl.timestamp("date_created")
+            .defaultTo(knex.fn.now());
+    })
+    .createTable("comments", (tbl) => {
+        tbl.increments();
+        tbl.integer("user_id")
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("users")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        tbl.integer("ticket_id")
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("tickets")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        tbl.string("comment")
+            .notNullable()
+        tbl.timestamp("timestamp")
+            .defaultTo(knex.fn.now());
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("comments")
+    .dropTableIfExists("tickets")
+    .dropTableIfExists("users")
 };
