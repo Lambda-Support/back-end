0b40e68fef55d5b615ce784aa1c9294e43289914
pdxdave <webdevdave@hotmail.com>
2021-06-29 14:13:30 -0700

    auth testing

---
+++ api/auth/auth-router.test.js
@@ -0,0 +1,42 @@
+const server = require('../server')
+const db = require('../../data/dbConfig')
+const request = require('supertest')
+
+
+describe('register', () => {
+
+    it('should give 201 status', async () => {
+        const res = await request(server)
+        .post("/users/register")
+        .send({ name: "Eddie Van Halen", username: "Jump", password: "Van Halen 1"})
+    expect(res.status).toBe(201)
+    })
+
+    it('should add a user and return it', async () => {
+        await request(server)
+        .post("/users/register")
+        .send({name: "David Lee Roth", username: "Panama", password: "Van Halen 2"})
+    expect({username: "David Lee Roth"})
+    })
+
+    beforeEach(async () => {
+        await db("users").truncate()
+    })
+})
+
+describe("user login", () => {
+
+    it('Should give a 200 status', async() => {
+        await request(server)
+        .post("/users/login")
+        .send({username: "Panama", password: "Van Halen 2"})
+    expect(200)
+    })
+
+    it('should respond with a token', async() => {
+        const res = await request(server)
+            .post("/users/login")
+            .send({username: "Panama", password: "Van Halen 2"})
+        expect(res.body.token)
+    })
+})
